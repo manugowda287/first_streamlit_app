6cedac4fc00096eeb6f2b0af4ff58d1bb955ff58
Manu J M <51666239+manugowda287@users.noreply.github.com>
2023-06-16 08:59:07 +0530

    Update streamlit_app.py
    added breakfast menu headers and 3 items!

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,8 @@
 import streamlit
 
 streamlit.title('My self Manu Gowda')
-streamlit.header('Hello')
-streamlit.text('I am from K R Pete')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
